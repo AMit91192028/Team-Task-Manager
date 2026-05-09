f3e960125d470465d445239a2aef1c142cf67d7f
AMit91192028 <amit31952352@gmail.com>
2026-05-09 19:52:42 +0530

    updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,8 +28,12 @@ app.use("/api/dashboard", dashboardRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.get('*name',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../public/index.html'))
-})
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 module.exports = app;
