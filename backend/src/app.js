f729adcd236de6b0d9e2f793e51508738879a3bb
AMit91192028 <amit31952352@gmail.com>
2026-05-09 20:40:58 +0530

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const { notFound, errorHandler } = require("./middlewares/erroe.middleware");
 const app = express();
 
 app.use(cors({
-  origin:['http://localhost:5173', 'http://localhost:3000'],
+  origin:['http://localhost:5173', 'http://localhost:3000'",
   credentials: true
 }));
 app.use(express.json());
--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://team-task-manager-52nx.onrender.com/api",
   withCredentials: true
 });
 
