3261942a44dd5d93be9933acab5c893d29b1b8c0
AMit91192028 <amit31952352@gmail.com>
2026-05-09 21:53:23 +0530

    updated app.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "/api";
-
+const baseURL = "https://team-task-manager-52nx.onrender.com/api";
 const axiosClient = axios.create({
   baseURL,
   withCredentials: true
